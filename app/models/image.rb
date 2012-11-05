0924b2e8057ce80cecc1f8e7e311b482465335c7
pvihtelic <patrick.vihtelic@gmail.com>
2012-11-04 23:42:58 -0600

    updating model syntax

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,5 @@
 class Image < ActiveRecord::Base
   attr_accessible :ski_id, :url
-
-  attr_accessible :ski_id, :url
   
   belongs_to :ski
 
--- app/models/ski.rb
+++ app/models/ski.rb
@@ -1,6 +1,8 @@
 class Ski < ActiveRecord::Base
   attr_accessible :ability_level, :brand, :description, :gender, :model_year, :name, :rocker_type, :ski_type, :store_id
 
-  has_many :images, :specs, :inventories
+  has_many :images
+  has_many :specs
+  has_many :inventories
   
 end
