2ba68222311e3d8938d69c72b6d75f20e59e3bed
pvihtelic <patrick.vihtelic@gmail.com>
2012-11-04 23:50:46 -0600

    replacing size_id with size_available throughout the app

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,5 +1,5 @@
 class Inventory < ActiveRecord::Base
-  attr_accessible :price, :size_id, :ski_id, :ski_url, :store_id
+  attr_accessible :price, :size_available, :ski_id, :ski_url, :store_id
 
   belongs_to :ski
   belongs_to :store
